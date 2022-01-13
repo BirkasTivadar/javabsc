7027587f679882671b3dffd21a1ad212f505c413
BirkasTivadar <birkastivadar@gmail.com>
2022-01-13 19:55:55 +0100

    Lista létrehozása Kész

--- src/main/java/arraylist/Freezer.java
+++ src/main/java/arraylist/Freezer.java
@@ -1,10 +1,10 @@
 package arraylist;
 
-//Gyakorlati feladat - A mélyhűtő tartalma
-//Egy háziasszony egy listán tartja nyilván, hogy mi található a mélyhűtőben. Amikor betesz valamit, felírja a lista végére, amikor pedig kivesz valamit onnan, akkor természetesen kihúzza a listáról. Implementáld ezt a Freezer osztályban! A main() metódusban példányosíts egy String típusú elemeket tartalmazó listát, add hozzá öt ételfajta nevét, majd írd ki a konzolra a lista tartalmát és a méretét! Ezután törölj két elemet, és ekkor is írd ki a konzolra ellenőrzésként a lista tartalmát és a méretét!
-
 import java.util.ArrayList;
 import java.util.List;
+
+//Gyakorlati feladat - A mélyhűtő tartalma
+//Egy háziasszony egy listán tartja nyilván, hogy mi található a mélyhűtőben. Amikor betesz valamit, felírja a lista végére, amikor pedig kivesz valamit onnan, akkor természetesen kihúzza a listáról. Implementáld ezt a Freezer osztályban! A main() metódusban példányosíts egy String típusú elemeket tartalmazó listát, add hozzá öt ételfajta nevét, majd írd ki a konzolra a lista tartalmát és a méretét! Ezután törölj két elemet, és ekkor is írd ki a konzolra ellenőrzésként a lista tartalmát és a méretét!
 
 public class Freezer {
 
--- src/main/java/arraylist/Second.java
+++ src/main/java/arraylist/Second.java
@@ -1,12 +1,11 @@
 package arraylist;
 
+import java.util.Arrays;
+import java.util.List;
 
 //Gyakorlati feladat - Minden második
 //        A Second osztály main() metódusában hozz létre egy listát a következő számokból, majd írj ki a konzolra közülük minden másodikat!
 //        A számok: 3, 9, 42, 67, 4, 0, 76, 14, 84, 35, 92
-
-import java.util.Arrays;
-import java.util.List;
 
 public class Second {
     public static void main(String[] args) {
