91185b4bfd02139b1a3cde1830ba4d087eb2a754
BirkasTivadar <birkastivadar@gmail.com>
2022-03-15 11:41:14 +0100

    Felsorolásos típus Kész

--- src/main/java/enumtype/position/Position.java
+++ src/main/java/enumtype/position/Position.java
@@ -5,7 +5,7 @@ public enum Position {
     DIRECTOR(1000000, "Oficina, secretario, coche"),
     JEFE(650000, "Oficina, secretario"),
     GERENTE(500000, "Coche, móvil"),
-    EMPLEAOD(300000,"Móvil");
+    EMPLEAOD(300000, "Móvil");
 
     private final int salary;
 
