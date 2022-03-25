96b7fc107f9fc3654b2abe30041d434e4dff02b8
BirkasTivadar <birkastivadar@gmail.com>
2022-03-25 20:06:50 +0100

    Főbb String metódusok Kész

--- src/main/java/stringmethods/url/UrlManager.java
+++ src/main/java/stringmethods/url/UrlManager.java
@@ -1,10 +1,6 @@
 package stringmethods.url;
 
-import java.util.Locale;
-
 public class UrlManager {
-
-    private final String PROTOCOLEND = "://";
 
     private String protocol;
     private Integer port;
@@ -56,7 +52,7 @@ public class UrlManager {
     }
 
     private void findHostFromUrl(String url) {
-        int startHostIndex = url.indexOf(PROTOCOLEND) + 3;
+        int startHostIndex = url.indexOf("://") + 3;
         int endHostIndex = url.indexOf(":", startHostIndex);
         if (endHostIndex < 0) {
             endHostIndex = url.indexOf("/", startHostIndex);
@@ -66,7 +62,7 @@ public class UrlManager {
     }
 
     private void findPathFromUrl(String url) {
-        int index = url.indexOf(PROTOCOLEND) + 3;
+        int index = url.indexOf("://") + 3;
         int startPathIndex = url.indexOf("/", index);
         if (startPathIndex < 0) {
             this.path = "";
@@ -82,54 +78,62 @@ public class UrlManager {
     }
 
     private void findPortFromUrl(String url) {
-        int index = url.indexOf(PROTOCOLEND);
-        int portStartIndex = url.indexOf(":", index);
+        int index = url.indexOf("://");
+        int portStartIndex = url.indexOf(":", index + 1);
         if (portStartIndex < 0) {
             return;
         }
-//        int portEndIndex = url.indexOf("/", portStartIndex);
-//        System.out.println(url.substring(portStartIndex + 1));
-//        this.port = portEndIndex < 0 ? Integer.valueOf(url.substring(portStartIndex + 1)) : Integer.valueOf(url.substring(portStartIndex + 1, portEndIndex));
+        int portEndIndex = url.indexOf("/", portStartIndex);
+        this.port = portEndIndex < 0 ? Integer.valueOf(url.substring(portStartIndex + 1)) : Integer.valueOf(url.substring(portStartIndex + 1, portEndIndex));
     }
 
     private void findProtocolFromUrl(String url) {
-        int index = url.indexOf(PROTOCOLEND);
+        int index = url.indexOf("://");
         this.protocol = url.substring(0, index).toLowerCase();
     }
 
     public static void main(String[] args) {
         String url = "HTTPS://EarthQuake.USgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+        System.out.println(url);
         UrlManager urlManager = new UrlManager(url);
 
-        System.out.println(urlManager.getProtocol());
-        System.out.println(urlManager.getHost());
-        System.out.println(urlManager.getPath());
-        System.out.println(urlManager.getPort());
+        System.out.println("Protocol: " + urlManager.getProtocol());
+        System.out.println("Host: " + urlManager.getHost());
+        System.out.println("Path: " + urlManager.getPath());
+        System.out.println("Port: " + urlManager.getPort());
+        System.out.println();
 
-//        String urlWithPortAtEnd = "HTTPS://EarthQuake.USgs.gov:80";
-//        urlManager = new UrlManager(urlWithPortAtEnd);
-//
-//        System.out.println(urlManager.getProtocol());
-//        System.out.println(urlManager.getHost());
-//        System.out.println(urlManager.getPath());
-////        System.out.println(urlManager.getPort());
-//
-//        String urlWithPort = "HTTPS://EarthQuake.USgs.gov:80/query?a=4";
-//        urlManager = new UrlManager(urlWithPort);
-//
-//        System.out.println(urlManager.getProtocol());
-//        System.out.println(urlManager.getHost());
-//        System.out.println(urlManager.getPath());
-////        System.out.println(urlManager.getPort());
-//
-//        String urlWithProperty = "HTTPS://EarthQuake.USgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
-//        urlManager = new UrlManager(urlWithProperty);
-//
-//        System.out.println(urlManager.hasProperty("format"));
-//        System.out.println(urlManager.getProperty("format"));
-//        System.out.println(urlManager.hasProperty("place"));
-//        System.out.println(urlManager.getProperty("place"));
-//        System.out.println(urlManager.hasProperty("time"));
-//        System.out.println(urlManager.getProperty("time"));
+
+        String urlWithPortAtEnd = "HTTPS://EarthQuake.USgs.gov:80";
+        System.out.println(urlWithPortAtEnd);
+        urlManager = new UrlManager(urlWithPortAtEnd);
+
+        System.out.println("Protocol: " + urlManager.getProtocol());
+        System.out.println("Host: " + urlManager.getHost());
+        System.out.println("Path: " + urlManager.getPath());
+        System.out.println("Port: " + urlManager.getPort());
+        System.out.println();
+
+
+        String urlWithPort = "HTTPS://EarthQuake.USgs.gov:80/query?a=4";
+        System.out.println(urlWithPort);
+        urlManager = new UrlManager(urlWithPort);
+
+        System.out.println("Protocol: " + urlManager.getProtocol());
+        System.out.println("Host: " + urlManager.getHost());
+        System.out.println("Path: " + urlManager.getPath());
+        System.out.println("Port: " + urlManager.getPort());
+        System.out.println();
+
+
+        String urlWithProperty = "HTTPS://EarthQuake.USgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+        urlManager = new UrlManager(urlWithProperty);
+
+        System.out.println(urlManager.hasProperty("format"));
+        System.out.println(urlManager.getProperty("format"));
+        System.out.println(urlManager.hasProperty("starttime"));
+        System.out.println(urlManager.getProperty("starttime"));
+        System.out.println(urlManager.hasProperty("endtime"));
+        System.out.println(urlManager.getProperty("endtime"));
     }
 }
