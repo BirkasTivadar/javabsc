60337b25b9da3d64a690bb0a09c13cd8ce905db0
BirkasTivadar <birkastivadar@gmail.com>
2022-04-15 18:20:19 +0200

    Kötelezően kezelendő kivétel a hívási láncban Kész

---
+++ src/main/java/introexceptioncheckedtrace/Underground.java
@@ -0,0 +1,21 @@
+package introexceptioncheckedtrace;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Underground {
+
+    public static void main(String[] args) {
+        Operations operations = new Operations();
+        List<String> wagonNumbers = null;
+
+        try {
+            wagonNumbers = operations.readFile();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        String dailySchedule = operations.getDailySchedule(wagonNumbers);
+        System.out.println(dailySchedule);
+    }
+}
