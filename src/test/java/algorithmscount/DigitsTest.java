abc4856bca64a9d0ee0d9bff03a005a659a83f21
BirkasTivadar <birkastivadar@gmail.com>
2022-04-18 14:02:33 +0200

    optimalizálás Kész

--- src/test/java/algorithmscount/DigitsTest.java
+++ src/test/java/algorithmscount/DigitsTest.java
@@ -2,7 +2,7 @@ package algorithmscount;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DigitsTest {
 
