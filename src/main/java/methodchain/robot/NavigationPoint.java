33f28bd81f0dd5360e7eb6650bae0feb34dab5b7
BirkasTivadar <birkastivadar@gmail.com>
2022-07-12 20:51:15 +0200

    Apró igazítás Kész

--- src/main/java/methodchain/robot/NavigationPoint.java
+++ src/main/java/methodchain/robot/NavigationPoint.java
@@ -2,7 +2,6 @@ package methodchain.robot;
 
 public record NavigationPoint(int distance, int azimut) {
 
-
     @Override
     public String toString() {
         return "distance: " + distance + " azimut: " + azimut;
