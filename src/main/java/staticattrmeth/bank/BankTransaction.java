e7665a8600ff82a8699943d0d67fb8839f379971
BirkasTivadar <birkastivadar@gmail.com>
2022-07-15 21:27:19 +0200

    Default és paraméter nélküli konstruktor Kész

--- src/main/java/staticattrmeth/bank/BankTransaction.java
+++ src/main/java/staticattrmeth/bank/BankTransaction.java
@@ -1,9 +1,27 @@
 /*
 Banki tranzakciók követése
         A BankTransaction osztály követi a létrehozott példányai által reprezentált banki tranzakciókat. Számolja a tranzakciókat, azok értékét összegzi, és utasításra átlagolja azokat. Minden tranzakció értékét megvizsgálja, és a nap elején beállított statikus currentMinValue és currentMaxValue változókban nyilvántartja az adott tranzakcióig előforduló minimális és maximális tranzakció értékeket.
+        Attribútumok:
+private static final long MIN_TRANSACTION_VALUE
+private static final long MAX_TRANSACTION_VALUE
+private static long countOfTransactions
+private static long sumOfTransactions
+private static long currentMinValue
+private static long currentMaxValue
+        Konstruktor:
+public BankTransaction(long transactionValue)
+        Publikus statikus metódusok:
+public static void initTheDay()
+public static long getAverageOfTransaction()
+public static long getCurrentMinValue()
+public static long getCurrentMaxValue()
+public static long getSumOfTransactions()
+        Publikus metódus:
+public long getTransactionValue()
         Amennyiben a létrejövő BankTransaction objektum tranzakció értéke kívül esik a konstansként megadott min és max határokon (1 és 10 000 000), IllegalArgumentException-t várunk a megfelelő értesítő szöveggel. A megoldás készüljön fel arra az (egyébként nem túl valószínű) esetre is, ha egyetlen érvényes tranzakció sem volt. Ilyenkor az averageOfTransaction, a getCurrentMinValue és a getCurrentMaxValue metódusok nullát adjanak vissza! Több esetben jól alkalmazható a Java három operandusú művelete!
 */
-package staticattrmeth.bank;
+
+        package staticattrmeth.bank;
 
 public class BankTransaction {
 
